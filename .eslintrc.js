6ac9724b82a835516bd8baec78d7675f9a5d44d2
Aaron Cunnington <azcn2503@gmail.com>
2017-12-22 17:54:05 +0000

    Updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
-  "extends": "airbnb",
-  "rules": {
+  extends: "airbnb",
+  rules: {
+    "arrow-parens": 0,
     "comma-dangle": 0,
     "import/no-extraneous-dependencies": 0,
-    "react/jsx-filename-extension": 0
+    "react/jsx-filename-extension": 0,
+    quotes: ["error", "double"]
   }
 };
