842b8b99f0dd0d22cb4be08aa0d9a31ba514cf3d
Aaron Cunnington <azcn2503@gmail.com>
2017-12-23 14:59:24 +0000

    Make cards api work again

--- src/server/api/card.js
+++ src/server/api/card.js
@@ -1,28 +1,16 @@
 class CardApi {
-  constructor() {
-    this.app = null;
-    this.logger = null;
-    this.service = null;
-  }
-
-  initialise({ app, logger, service }) {
-    this.app = app;
-    this.logger = logger;
-    this.service = service;
-
+  initialise({ app, logger, cardService }) {
     app.get("/api/subject/:subjectId/cards", (req, res) => {
       logger.info("Getting cards");
-      this.service
-        .getCards(req.params.subjectId)
-        .then(cards => res.json(cards));
+      cardService.getCards(req.params.subjectId).then(cards => res.json(cards));
     });
 
     app.post("/api/card", (req, res) => {
       logger.info("Adding card");
-      this.service.addCard(req.body.card).then(card => res.json(card));
+      cardService.addCard(req.body.card).then(card => res.json(card));
     });
 
-    this.logger.debug("Cards API initialised");
+    logger.debug("Cards API initialised");
   }
 }
 
